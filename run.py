2ceda302c78610e61ab219617eaaa3dfdb12d909
robinoldman <robin.c.oldman@gmail.com>
2023-01-27 16:25:13 +0000

    add doc strings to each part

--- run.py
+++ run.py
@@ -1,16 +1,27 @@
-
 from pyfiglet import Figlet
 import os
+import time
 
 def clear_screen():
+    """Clears the terminal screen."""
     os.system('clear' if os.name == 'posix' else 'cls')
 
+    """add art graphicsd to the welcome screen """
 custom_fig = Figlet(font='rozzo')
 print(custom_fig.renderText('Battleship!'))
 
-import time
 
-name = input("What's your name? ")
+    """intro words to the welcome screen, typed in slow motion"""
+intro_string = ("War is upon us! Only the strongest can suvive!\n"
+
+"Do you have the skills to defeat your enemy?\n")
+
+for char in intro_string:
+    print(char, end="", flush=True)
+    time.sleep(0.1)
+
+    """user enters name"""
+name = input("What's your name ally? ")
 
 for char in name:
     print(char, end="", flush=True)
@@ -22,6 +33,10 @@ for char in welcome_string:
     print(char, end="", flush=True)
     time.sleep(0.1)
 
+    """pause between new page"""
+time.sleep(2) 
+
+    """clears screen ready for game""""
 clear_screen()
 
 board = []
@@ -31,6 +46,7 @@ for i in range(5):
     board.append([str(i+1)] + ["0"]*5)
 
 def print_board(board):
+    """Prints the board in the terminal screen."""
     for i in board:
         print(" ".join(i))
 
