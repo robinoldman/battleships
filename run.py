53b766784df8328dffab47b5e0c8ca8867f3d34e
robinoldman <robin.c.oldman@gmail.com>
2023-01-27 15:41:13 +0000

    add graphics to intro page

--- run.py
+++ run.py
@@ -1,11 +1,31 @@
 
+from pyfiglet import Figlet
+import os
+
+def clear_screen():
+    os.system('clear' if os.name == 'posix' else 'cls')
+
+custom_fig = Figlet(font='rozzo')
+print(custom_fig.renderText('Battleship!'))
+
+import time
+
+name = input("What's your name? ")
+
+for char in name:
+    print(char, end="", flush=True)
+    time.sleep(0.1)
+    
+welcome_string = " Welcome, " + name + "!"
+
+for char in welcome_string:
+    print(char, end="", flush=True)
+    time.sleep(0.1)
+
+clear_screen()
 
 board = []
 
-    """
-    board, row of numbers at the top folowwed by loop of 0s starting with 
-    1 and then adding the concecutive number as it goes
-    """
 print (" ", 1,2,3,4,5)
 for i in range(5):
     board.append([str(i+1)] + ["0"]*5)
