474d31fa6e56530a78f9477af69252c697848e1d
robinoldman <robin.c.oldman@gmail.com>
2023-01-27 12:05:18 +0000

    add board with 0s

--- run.py
+++ run.py
@@ -1,3 +1,9 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+board =[]
+for i in range (0,5):
+    board.append([str(0)]*5)
+
+def print_board(board):
+    for i in board:
+        print (" ".join(i))
+
+print_board(board)
