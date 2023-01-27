92be6e809408ee3b5454e1af13eaee04411223c7
robinoldman <robin.c.oldman@gmail.com>
2023-01-27 14:56:46 +0000

    add numbers vertically and horizontally

--- run.py
+++ run.py
@@ -1,9 +1,17 @@
-board =[]
-for i in range (0,5):
-    board.append([str(0)]*5)
+
+
+board = []
+
+    """
+    board, row of numbers at the top folowwed by loop of 0s starting with 
+    1 and then adding the concecutive number as it goes
+    """
+print (" ", 1,2,3,4,5)
+for i in range(5):
+    board.append([str(i+1)] + ["0"]*5)
 
 def print_board(board):
     for i in board:
-        print (" ".join(i))
+        print(" ".join(i))
 
 print_board(board)
